2b4cdb16b69326437e0df0b5011ce44cf8d09335
Ovie G Bazunu <obazunu17@gmail.com>
2023-10-17 01:10:54 +0100

    Design array has been added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,11 +12,13 @@ import githubLogo from "../assets/GitHub-icon.png";
 import vercelLogo from "../assets/vercel-icon.png";
 import framerLogo from "../assets/framerMotion-icon.jpg";
 import sassLogo from "../assets/Sass-icon.png";
+import tailwindLogo from "../assets/tailwind-icon.png";
 
 const projectData = [
   {
     title: "GiftKart",
-    details: "A E-commerce site built using Vite & React ",
+    details:
+      "A E-commerce site for buying and selling items. Built using Vite & React with the storage and authentication handled by Firebase and payment handled by Stripe ",
     frontEnd: "Vite, React, Sass, Redux, MUI",
     backEnd: "Firebase Authentication, Firebase Storage, Stripe",
     img: ecomWebsiteImg,
@@ -59,12 +61,21 @@ const projectData = [
         logo: framerLogo,
       },
     ],
+    design: [
+      { color: "#9DC4B5" },
+      { color: "#d6e5fb" },
+      { color: "#ceebe9" },
+      { color: "#e2f2b2" },
+      { color: "#0a1d37" },
+      { color: "#BAD9A2" },
+      { color: "#484A39" },
+    ],
   },
   {
     title: "Koopa Burgers",
-    details: "A E-commerce site built using Vite & React ",
+    details: "A burger website created using vite & react along with MUI 5 ",
     frontEnd: "Vite, React, Sass, Redux, MUI",
-    backEnd: "Firebase Authentication, Firebase Storage, Stripe",
+    backEnd: "N/A",
     img: foodWebsiteImg,
     websiteUrl: "https://koopa-burgers.vercel.app",
     techStack: [
@@ -89,6 +100,30 @@ const projectData = [
         logo: vercelLogo,
       },
     ],
+    design: [{ color: "#E78C49" }, { color: "#FFF" }, { color: "#121619" }],
+  },
+  {
+    title: "Ovie G Bazunu Portfolio",
+    details:
+      "A portfolio for Ovie Bazunu detailing his projects, work timeline and employment information. Made using Vite with react along with tailwind css",
+    frontEnd: "Vite, React, Tailwind, MUI",
+    backEnd: "N/A",
+    img: portfolioImg,
+    websiteUrl: "https://oviebazunuportfolio.vercel.app",
+    techStack: [
+      { name: "React", logo: reactLogo },
+      { name: "Vite", logo: viteLogo },
+      { name: "Tailwind", logo: tailwindLogo },
+      { name: "MUI 5", logo: mui5Logo },
+      { name: "Github", logo: githubLogo },
+      { name: "Vercel", logo: vercelLogo },
+    ],
+    design: [
+      { color: "#312854" },
+      { color: "#343278" },
+      { color: "#FFF" },
+      { color: "#78716c" },
+    ],
   },
 ];
 
@@ -98,8 +133,8 @@ const Projects = () => {
       <h1 className="text-4xl font-bold text-center text-[#353278]">
         Projects
       </h1>
-      <p className="text-center text-base py-8">
-        The most recent projects which i have done. All of these projects range
+      <p className="text-center text-base text-stone-700 py-8 ">
+        The most recent projects which I have done. All of these projects range
         from using different parts of vite and react along with different tools
         for payment and authentication such as Firebase and Stripe.
       </p>
@@ -114,6 +149,7 @@ const Projects = () => {
             image={item.img}
             websiteUrl={item.websiteUrl}
             techStack={item.techStack}
+            design={item.design}
           />
         ))}
       </div>
--- src/components/items/ProjectItem.jsx
+++ src/components/items/ProjectItem.jsx
@@ -8,10 +8,11 @@ const ProjectItem = ({
   image,
   websiteUrl,
   techStack,
+  design,
 }) => {
   return (
     <>
-      <div class="lg:wrap pt-10 lg:flex">
+      <div class="lg:wrap pt-10 lg:flex mt-[40px]">
         <div>
           <a
             className="border-b-4 border-b-[#312854] text-3xl font-bold hover:opacity-70 text-[#353278]"
@@ -26,11 +27,11 @@ const ProjectItem = ({
           <h2 className="text-xl font-semibold text-[#312854]">Back End</h2>
           <p className="pb-4 text-base text-stone-700">{backEnd}</p>
           <a
-            className="inline-block text-base px-2 py-1 font-semibold text-white bg-[#312854] hover:opacity-70 rounded-md mb-2"
+            className="inline-block text-base px-2 py-1 font-semibold text-white bg-[#312854] hover:opacity-70 rounded-md mb-4"
             href={websiteUrl}
             target="_blank"
           >
-            Vist Website
+            Visit Website
           </a>
         </div>
         <div className="mb-5 h-fit border object-contain shadow-md lg:w-6/12 ml-auto">
@@ -47,6 +48,30 @@ const ProjectItem = ({
               <img src={tech.logo} alt={tech.name} className="w-16 h-16 mt-4" />
               <p className="mt-4 text-center font-bold text-stone-700">
                 {tech.name}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h1 className="max-w-fit border-b-4 border-b-[#312854] text-2xl font-bold text-[#343278] mt-12">
+          Design
+        </h1>
+        <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 lg:grid-cols-6">
+          {design.map((color, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <div
+                style={{
+                  backgroundColor: color.color,
+                  marginTop: "20px",
+                  boxShadow: "2px 2px 5px rgba(0,0,0,0.4)",
+                  width: "80px",
+                  height: "80px",
+                  borderRadius: "50%",
+                }}
+              ></div>
+              <p className="mt-4 text-center font-bold text-stone-700">
+                {color.color}
               </p>
             </div>
           ))}
